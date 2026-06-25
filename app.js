33a3c6d6e596a5d5c6ca222b88382fdf1a0fd5d1
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2026-06-26 02:57:55 +0300

    corrected background

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ let tabHistory = [];
 let allowLeavingPage = false;
 let dashboardRefreshTimer = null;
 
-const APP_VERSION = "28";
+const APP_VERSION = "29";
 let updateCheckTimer = null;
 
 document.addEventListener("DOMContentLoaded", init);
