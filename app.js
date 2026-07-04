d0423b9de4196a04b1c83202807276107756de30
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2026-07-04 21:54:10 +0300

    fixed colors of final

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ let tabHistory = [];
 let allowLeavingPage = false;
 let dashboardRefreshTimer = null;
 
-const APP_VERSION = "38.7";
+const APP_VERSION = "38.8";
 let updateCheckTimer = null;
 const SITE_STAGE_CACHE_KEY = "wcSiteStage";
 
